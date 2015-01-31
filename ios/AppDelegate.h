bd188045ce8071d16b35437850cdde274241345f
keepcalm444 <keepcalm444@gmail.com>
2015-01-31 19:19:14 +1100

    Initial Commit

---
+++ ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios
+//
+//  Created by Simon on 31/01/2015.
+//  Copyright (c) 2015 sbhstimetable.tk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ios
+//
+//  Created by Simon on 31/01/2015.
+//  Copyright (c) 2015 sbhstimetable.tk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
